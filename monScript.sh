f79751d65df40afa789d329e52b775fc070fd410
Hadronoxx <83581005+Hadronoxx@users.noreply.github.com>
2022-01-17 15:17:23 +0100

    Update monScript.sh

--- monScript.sh
+++ monScript.sh
@@ -11,3 +11,37 @@ echo $pays
 for i in "${pays[@]}
 
 echo "$tab"
+
+# Maxime pas complet
+
+#créer un fichier résultat
+mkdir -p Resultat/$1
+cd ..
+
+
+test=`grep -lr $1`
+test=`grep -le "Year $fichier"`
+echo "$fichier"
+
+bool=false
+cpt=0
+tab=`head -n1 "$PWD/ProjetShell/Source/Country_Consumption_TWH.csv"`
+IFS=',' read -ra pays <<< "$tab"
+paysCherche="$1"
+nbColonnes=45
+while [ $bool = false ] && [ $cpt -le $nbColonnes ]
+do
+if [[ ${pays[$cpt]} == $paysCherche ]]
+then
+bool=true
+fi
+let "cpt++"
+done
+if [ $cpt -gt $nbColonnes ]
+then
+echo "le pays n'existe pas"
+else
+echo "le pays existe à la colonne $cpt"
+fi
+
+echo "$tab"
